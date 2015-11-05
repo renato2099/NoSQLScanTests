38600f7e04ffcd1b3fda911b71b8a8d340b2fce7
Renato Marroquin <renatoj.marroquin@gmail.com>
2015-11-05 16:58:44 +0100

    Running on server

--- cpp/benchmark.hpp
+++ cpp/benchmark.hpp
@@ -20,59 +20,88 @@
 
 using namespace RAMCloud;
 
+const int session_timeout = 100000;
+const std::string tName = "employees";
+
    class Benchmark
    {
    	private:
    		void rcInsert(int nInserts, long idRange, uint64_t tableId, std::string locator, std::string clName, bool verbose);
    		
-   		// uint64_t createTable(RamCloud client);
+   		uint64_t createTable(RamCloud* client);
    	public:
    		void load(int nThreads, int nInserts, std::string locator, std::string clName, bool verbose);
-   		void scan(double percentage);
+   		void scan(double percentage, std::string locator, std::string clName);
 
    };
 
-   /*uint64_t Benchmark::createTable(RamCloud client) {
-   	std::string locator = "";
-   	std::string clName = "";
-   	std::string tableName = "employees";
+   void Benchmark::scan(double percentage, std::string locator, std::string clName) {
+   	Context context(false);
+   	context.transportManager->setSessionTimeout(session_timeout);
+   	RamCloud client(&context, locator.c_str(), clName.c_str());
+	uint64_t tableId = 0;
+	try {
+		tableId = client.getTableId(tName.c_str());
+		Buffer objects, state;
+		uint64_t tableFirstHash;
+		client.enumerateTable(tableId, false, tableFirstHash, state, objects);
+		//while (tableFirstHash != 0) {
+		//	client.enumerateTable(tableId, false, tableFirstHash, state, objects);
+			// do something clever with the data
+		//}
+	} catch (ClientException &e) {
+		std::cout << "[Scan] Something went wrong while trying to scan." << std::endl;
+	}
+   }
+
+   uint64_t Benchmark::createTable(RamCloud* client) {
    	uint64_t tableId = 0;
-   	// tableId = client->getTableId(tableName);
+	std::string tableName = "employees";
+	try
+	{
+   		tableId = client->getTableId(tName.c_str());
+	} catch (ClientException &e) 
+	{
+		std::cout<<"[Load] Table doesn't exist. It will be created." << std::endl;
+	}
    	if (tableId == 0) {
-   		std::cout<<"[Load] Table non-existing" << std::endl;
-   		std::cout<<"[Load] Creating table" << tableName << std::endl;
-   		// tableId = client->createTable(tableName);
+   		std::cout<<"[Load] Creating table" << tName << std::endl;
+   		tableId = client->createTable(tName.c_str());
    	} else {
    		std::cout<<"[Load] Table exists. Reusing it." << std::endl;
-   		// cient->dropTable(tableName);
+   		//client->dropTable(tableName);
    	}
    	return tableId;
-   }*/
+   }
 
-   void Benchmark::load(int nThreads, int nInserts, std::string locator, std::string clName, bool verbose) {
+   void Benchmark::load(int nThreads, int nInserts, std::string coordLocator, std::string clusterName, bool verbose) {
    	typedef std::chrono::high_resolution_clock clock;
 	typedef std::chrono::milliseconds milliseconds;
 
-	// Context context(false);
-   	// context.transportManager->setSessionTimeout(100000);
-	// RamCloud client(&context, locator.c_str(), clName.c_str());
+   	if (verbose) 
+	{
+		std::cout << "[Load] Cluster: " << clusterName << " ServiceLocator: " << coordLocator <<  std::endl;
+   		std::cout << "[Load] TotalOps: " << nInserts << " with " << nThreads << " threads." <<  std::endl;
+	}
+
+	Context context(false);
+   	context.transportManager->setSessionTimeout(session_timeout);
+
+	RamCloud client(&context, coordLocator.c_str(), clusterName.c_str());
 	uint64_t tableId = 0;
-	// tableId = createTable(client);
+	tableId = createTable(&client);
 
    	clock::time_point t0 = clock::now();
    	std::thread* tids = new std::thread[nThreads];
-
-   	if (verbose)
-   		std::cout << "[Load] " << nInserts << " with " << nThreads << " threads." << std::endl;
 
    	int threadOp = nInserts/nThreads;
    	long idRange = 1;
    	for (int i = 0; i < nThreads-1; i++)
 	{
-		tids[i] = std::thread(&Benchmark::rcInsert, this, threadOp, idRange, tableId, locator, clName, verbose);
+		tids[i] = std::thread(&Benchmark::rcInsert, this, threadOp, idRange, tableId, coordLocator, clusterName, verbose);
 		idRange += threadOp;
 	}
-	tids[nThreads-1] = std::thread(&Benchmark::rcInsert, this, (threadOp+nInserts%nThreads), idRange, tableId, locator, clName, verbose);
+	tids[nThreads-1] = std::thread(&Benchmark::rcInsert, this, (threadOp+nInserts%nThreads), idRange, tableId, coordLocator, clusterName, verbose);
 
    	for (int i = 0; i < nThreads; i++)
 	{
@@ -86,24 +115,21 @@ using namespace RAMCloud;
    void Benchmark::rcInsert(int nInserts, long idRange, uint64_t tableId, std::string locator, std::string clName, bool verbose) 
    {
    	int cnt = 0;
-   	std::cout<<std::endl;
-
+   	tpcc::Random_t random;
+	std::string table = "employees";
    	Context context(false);
    	context.transportManager->setSessionTimeout(100000);
    	RamCloud client(&context, locator.c_str(), clName.c_str());
 
-   	std::stringstream record;
-   	tpcc::Random_t random;
-   	float salary = random.randomWithin(0.0, 1.0);
-   	if (salary < 0) salary *=-1;
-
-   	record << random.astring(15, 15) << random.astring(20, 20) <<  salary  << random.random(1, 100) << random.astring(5,5); 
-   	// std::cout << record.str();
-
    	while(cnt < nInserts) {
-   		std::cout << "Inserted element: " << idRange << std::endl;
-   		std::string val = "";
-   		// client.write(table, idRange, downCast<uint16_t>(idRange), val, downCast<uint32_t>(strlen(val)+1));
+   		//std::cout << "Inserted element: " << idRange << std::endl;
+   		std::stringstream val;
+   		float salary = random.randomWithin(0.0, 1.0);
+   		if (salary < 0) salary *=-1;
+		val << random.astring(15, 15) << random.astring(20, 20) <<  salary  << random.random(1, 100) << random.astring(5,5);
+		const char * vv = val.str().c_str();
+   		client.write(tableId, (const void*)&idRange, sizeof(idRange), vv, downCast<uint32_t>(strlen(vv)+1));
+   		//client.write(tableId, vali.c_str(), downCast<uint16_t>(idRange), val.c_str(), downCast<uint32_t>(strlen(val.c_str())+1));
    		if (verbose)
    			if ((cnt*100 % nInserts)/10 == 0)
    				std::cout << "[Load] Inserted " << idRange << std::endl;
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -53,7 +53,7 @@ struct Arg: public option::Arg
   }
 };
 
-enum  optionIndex { UNKNOWN, HELP, VERBOSE, THREADS, OPERATIONS, TABLE, CLUSTER };
+enum  optionIndex { UNKNOWN, HELP, VERBOSE, THREADS, OPERATIONS, TABLE, CLUSTER, LOAD, PERCENTAGE };
 const option::Descriptor usage[] =
 {
 	{UNKNOWN,		0,	"", 	"",		 		Arg::None, 		"USAGE: example [options]\n\n"
@@ -62,12 +62,14 @@ const option::Descriptor usage[] =
 	{VERBOSE,  		0,	"v", 	"verbose",		Arg::None, 		"  --verbose \t \t -v \t \t Print information about the benchmark." },
 	{THREADS,   	0,	"t", 	"threads",		Arg::Numeric, 	"  --threads <num> \t \t -t <num>\t \t Number of threads." },
 	{OPERATIONS,	0,	"o",	"operations",	Arg::Numeric,	"  --operations <num> \t \t -o <num> \t \t Number of total operations."},
-	{TABLE,	0,	"tb",	"table",	Arg::Numeric,	"  --table <tableName> \t \t -tb <tableName> \t \t Table name."},
-	{CLUSTER,	0,	"c",	"cluster",	Arg::Numeric,	"  --cluster <clusterName> \t \t -c <clusterName> \t \t Table name."},
+	{TABLE,	0,	"r",	"relation",	Arg::Required,	"  --relation <relationName> \t \t -r <relationName> \t \t Relation name."},
+	{LOAD,	0,	"l",	"load",	Arg::None,	"  --load <load> \t \t -l <load> \t \t To load data into table."},
+	{PERCENTAGE,	0,	"p",	"percentage",	Arg::Numeric,	"  --percentage <percentage> \t \t -p <percentage> \t \t Percentage in terms of salary."},
+	{CLUSTER,	0,	"c",	"cluster",	Arg::Required,	"  --cluster <clusterName> \t \t -c <clusterName> \t \t Cluster name."},
 	{0,0,0,0,0,0}
 };
 
-int readCmdLine(int argc, char** argv, int &numThreads, int &numOperations, bool &verbose)
+int readCmdLine(int argc, char** argv, int &numThreads, int &numOperations, std::string &tableName, std::string &clusterName, double &percentage, bool &load, bool &verbose)
 {
 	// program options
 	// program options
@@ -97,10 +99,22 @@ int readCmdLine(int argc, char** argv, int &numThreads, int &numOperations, bool
 			// UNKNOWN, HELP, BENCHMARK, POP, RM, THRDS, INSERTS, FIXED, VERBOSE, LOCK_FREE, CNC_TBB
 			case HELP:
 				// not possible, because handled further above and exits the program
+			case LOAD:
+				load = true;
+				break;
+			case PERCENTAGE:
+				char *endptr;
+				percentage = std::strtod(opt.arg, &endptr);
+				break;
 			case VERBOSE:
 				verbose = true;
-				// std::cout<<opt.arg<<std::endl;
 				//istringstream(opt.arg) >> std::boolalpha >> verbose;
+				break;
+			case TABLE:
+				tableName = opt.arg;
+				break;
+			case CLUSTER:
+				clusterName = opt.arg;
 				break;
 			case THREADS:
 				strValue << opt.arg;
@@ -118,8 +132,19 @@ int readCmdLine(int argc, char** argv, int &numThreads, int &numOperations, bool
 	return 0;
 }
 
-int check_parameters(int numThreads, int numOperations, bool verbose)
+int check_parameters(int numThreads, int numOperations, std::string tableName, std::string clusterName, double percentage, bool load, bool verbose)
 {
+	//todo some validation over percentage and load
+	if (tableName.empty()) 
+	{
+		cerr << "A tableName must be specified." << endl;
+		return 1;
+	}
+	if (clusterName.empty())
+	{
+		cerr << "A clusterName must be specified." << endl;
+		return 1;
+	}
 	if (numThreads < 1)
 	{
 		cerr << "The number of threads has be a positive integer number." << endl;
@@ -137,22 +162,25 @@ int check_parameters(int numThreads, int numOperations, bool verbose)
 
 int main(int argc, char** argv) 
 {
-	bool verbose = false;
+	bool verbose = false, load = false;
 	int numThreads = DEFAULT_THREADS, numOperations = DEFAULT_OPS;
-	std::string tName = "employees";
-	std::string clName = "cluster";
+	std::string tableName = "employees";
+	std::string clusterName = "cluster";
+	double percentage = 0.5;
 
-	if (!readCmdLine(argc, argv, numThreads, numOperations, verbose))
+	if (!readCmdLine(argc, argv, numThreads, numOperations, tableName, clusterName, percentage, load, verbose))
 	{
-		if (check_parameters(numThreads, numOperations, verbose)) {
+		if (check_parameters(numThreads, numOperations, tableName, clusterName, percentage, load, verbose)) {
 			exit(1);
 		}
 		Benchmark bench;
-		std::cout << "Loading database" << std::endl;
-		bench.load(numThreads, numOperations, tName, clName, verbose);
+		if (!load) 
+		{
+			std::cout << "Loading database" << std::endl;
+			bench.load(numThreads, numOperations, tableName, clusterName, verbose);
+		}
 		std::cout << "Scanning database" << std::endl;
-		// bench.scan();
+		bench.scan(percentage, tableName, clusterName);
 	}
-	std::cout << "verbose" << std::endl;
 	return 0;
 }
